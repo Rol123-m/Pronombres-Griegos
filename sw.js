e65ccb5048be5c37dadaa1b6b6350035e8290779
Rol123-m <rol123-m.github.io.com>
2026-04-22 00:17:44 -0400

    first commit

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'koine-master-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  // ⚠️ Agrega aquí tus archivos CSS, JS e imágenes si los tienes separados
+  // './css/style.css',
+  // './js/app.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
